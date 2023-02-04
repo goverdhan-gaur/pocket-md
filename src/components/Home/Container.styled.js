684c79e63955ddff448ea2058ec13e71b0a0e32f
Goverdhan Gaur <84145630+goverdhan-gaur@users.noreply.github.com>
2023-02-04 01:05:30 -0500

    feat: implemented Components structure

---
+++ src/components/Home/Container.styled.js
@@ -0,0 +1,7 @@
+const { default: styled } = require('styled-components')
+
+export const Container = styled.div`
+  width: 100%;
+  height: 20px;
+  background: red;
+`
---
+++ src/components/Home/Container.tsx
@@ -0,0 +1,5 @@
+import * as Styled from './Container.styled'
+
+export default function HomePage() {
+  return <Styled.Container>Sahgil</Styled.Container>
+}
