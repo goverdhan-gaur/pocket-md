8c20340d0c973422da6334f9f7d013b464245303
Goverdhan Gaur <84145630+goverdhan-gaur@users.noreply.github.com>
2023-02-14 09:18:47 -0500

    feat: add mixins for styled components

--- src/mixins/getRemFontSize.ts
+++ src/mixins/getRemFontSize.ts
@@ -1,4 +1,4 @@
 export const px = (size: number, base = 16) => `
-  font-size: ${size}px; // older browsers fallback
+  font-size: ${size}px;
   font-size: calc(${size / base} * 1rem);
 `;
---
+++ src/mixins/mediaQuery.ts
@@ -0,0 +1,14 @@
+import { css } from 'styled-components';
+
+const sizes = {
+    Mobile: '576px',
+    Tablet: '768px',
+    Desktop: '992px',
+    LargeDesktop: '1200px',
+};
+
+export const mediaQuery = (label: keyof typeof sizes, content: any) => css`
+  @media (max-width: ${sizes[label]}) {
+    ${content}
+  }
+`;
